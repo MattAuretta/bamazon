c6f79fdbe1afe478a845db80e5d7110dfcda8858
Matt Auretta <mauretta2@gmail.com>
2018-10-07 20:17:44 -0600

    functional addInventory

--- bamazonManager.js
+++ bamazonManager.js
@@ -71,11 +71,54 @@ function viewLowInventory() {
     connection.query(query, function (err, response) {
         //Loop through each item
         for (var i = 0; i < response.length; i++) {
-            //Log each item name and quantity
+            //Log each item id, name, and quantity
             console.log("Item ID: " + response[i].item_id + " || Product: " + response[i].product_name + " || Quantity: " + response[i].stock_quantity);
         };
         console.log("\n---------------------\n");
         //Run main function again
         runManager();
     });
+};
+
+function addInventory() {
+    inquirer
+        .prompt([{
+                name: "id",
+                type: "input",
+                message: "Enter the ID of the product you wish to add inventory to: ",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                name: "units",
+                type: "input",
+                message: "Enter the amount of units you would like to add: ",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    };
+                    return false;
+                }
+            }
+        ]).then(function (answer) {
+            var query = "SELECT item_id, product_name, stock_quantity FROM products WHERE ?";
+            connection.query(query, {
+                    item_id: answer.id
+                },
+                function (err, response) {
+                        //Variable to hold logic for updated stock
+                        var updateStock = parseInt(response[0].stock_quantity) + parseInt(answer.units);
+                        //Update stock quantity in database
+                        connection.query("UPDATE products SET stock_quantity = " + updateStock + " WHERE ?", {
+                            item_id: answer.id
+                        });
+                        //Display total cost
+                        console.log("\nYour updated inventory of " + response[0].product_name + " is: " + updateStock + "\n");
+                        runManager();
+                });
+        });
 }
