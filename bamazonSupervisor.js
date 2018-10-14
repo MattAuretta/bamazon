76213b52d0c205646c80738a398573a469de3433
Matt Auretta <mauretta2@gmail.com>
2018-10-13 19:42:33 -0600

    cli-table working in supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -1,7 +1,7 @@
 //Require npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var cTable = require("console.table");
+var Table = require("cli-table");
 
 //Create connection to MySQL database
 var connection = mysql.createConnection({
@@ -43,16 +43,22 @@ function runSupervisor() {
 };
 
 function viewByDepartment() {
-    var query = "SELECT departments.department_id, products.department_name, departments.over_head_costs, SUM(products.product_sales), departments.department_name, "
+    var query = "SELECT departments.department_id, products.department_name, departments.over_head_costs, SUM(products.product_sales) AS product_sales, departments.department_name, "
     query += "SUM(products.product_sales - departments.over_head_costs) AS total_profit ";
     query += "FROM products INNER JOIN departments ON (products.department_name = departments.department_name) GROUP BY departments.department_id ORDER BY departments.department_id";
     connection.query(query, function (err, res) {
-        var inventory = [];
+        console.log(res);
+        var tableArray = [];
+        var table = new Table ({
+            head: ["Department Id", "Department Name", "Over Head Costs", "Product Sales", "Total Profit"],
+            colWidths: [20, 20, 20, 20, 20]
+        });
+        
         for (var i = 0; i < res.length; i++) {
-            inventory.push(res[i])
+            table.push([res[i].department_id, res[i].department_name, res[i].over_head_costs, res[i].product_sales, res[i].total_profit]);
         }
         console.log("\n")
-        console.table(inventory);
+        console.log(table.toString());
         runSupervisor();
     });
 };
